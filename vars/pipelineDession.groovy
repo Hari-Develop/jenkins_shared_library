80367a318fddf01f1ea1fe2375cbf81a760d4b04
hari <hari@gmail.com>
2024-02-16 13:22:43 +0530

    file chnages

---
+++ vars/pipelineDession.groovy
@@ -0,0 +1,18 @@
+#!groovy
+
+def decidePipeline(Map configMap){
+    application = configMap.get("application")
+    switch(application) {
+        case "nodejs"
+            nodejs(configMap) 
+            break
+        case "java"
+            java(configMap)
+            break
+        case "nodeEKS"
+            nodeEKS(configMap)
+            break
+        default:
+            error "The application pipline is not found"
+    }
+}
