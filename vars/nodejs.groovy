1c600e43f17994b64982641f9de02c55f47a6760
hari <hari@gmail.com>
2024-02-21 11:58:47 +0530

    file changes

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -1,98 +1,43 @@
-def call(Map configMap) {
-
-    pipeline {
-
+def call(Map configMap){
+    pipeline{
         agent {
             node {
-                label 'Jenkins_agent'
-            } 
+                label "Jenkins_agent"
+            }
         }
         environment {
-            // using the environment variables here
-            PackageVersion = ''
-            //nexusURL = '172.31.32.58:8081'
+            packageVersion = "${packageJSON.version}"
         }
         stages {
-            stage("Getting the version of the file") {
+            stage('getting the version of the application') {
                 steps {
                     script {
-                        def Package_version = readJSON file: 'package.json'
-                        PackageVersion = Package_version.version
-                        echo "application version: $PackageVersion"
-                        echo "application URL: $nexusURL"
+                        def packageJSON = readJSON file: 'package.json'
+                        echo "print the package ${packageJSON.version}"
                     }
                 }
             }
-            stage('installing dependencies') {
+            stage("installing Dependences for projectVersion-${packageJSON.version}"){
                 steps {
-                    // Add npm steps here
                     sh """
-                        echo installing npm dependencies 
-                        pwd
                         npm install
-                        """ 
+                    """
                 }
             }
-            stage('Building Application') {
-                steps {
-                    // Add npm steps here
+            stage("unit test for projectVersion-${packageJSON.version}"){
+                steps{
                     sh """
-                        ls -la
-                        zip -q -r ${configMap.component}.zip ./* -x jenkinsfile -x ".git" -x "*Jenkinsfile" -x "*.zip"
-                        ls -a
-                        """ 
+                        echo "Here we will perfume the unit test"
+                    """
                 }
             }
-            stage('punshing the arifactory into nexus') {
-                steps {
-                        nexusArtifactUploader(
-                            nexusVersion: 'nexus3',
-                            protocol: 'http',
-                            nexusUrl: "${nexusURL}",
-                            groupId: 'com.roboshop',
-                            version: "${PackageVersion}",
-                            repository: '${configMap.component}',
-                            credentialsId: 'nexus-cred',
-                            artifacts: [
-                                [artifactId: '${configMap.component}',
-                                classifier: '',
-                                file: '${configMap.component}.zip',
-                                type: 'zip']
-                            ]
-                        )
+            stage("Building the projectVersion-${packageJSON.version}"){
+                steps{
+                    sh """
+                        echo "Build will run here"
+                    """
                 }
-            }
-            stage('Deploy') {
-                    steps {
-                        script {
-                            def params = [
-                                string(name: 'version' , value: "$PackageVersion"),
-                                string(name: 'environment' , value: "dev")
-                            ]
-                            build job: '${configMap.component}-deploy' , wait: true , parameters: params
-                        }
-                    }
-            }
-            stage('release') {
-                steps {
-                    // Add release steps here
-                    sh 'echo "releasing the application..."'
-                }
-            }
-            
-        }
-        post {
-            always {
-                echo "pipeline is running...."
-                deleteDir()
-            }
-            failure {
-                echo "pipeline is failure..."
-            }
-            success {
-                echo "pipeline is success...party..ledha..pushpa.."
             }
         }
-    } 
-
+    }
 }
--- vars/pipelineDession.groovy
+++ vars/pipelineDession.groovy
@@ -16,3 +16,49 @@ def decidePipeline(Map configMap){
             error "The application pipline is not found"
     }
 }
+
+def decidePipeline(Map configMap){
+    application = configMap.get("application")
+    switch(application) {
+        case "nodejs":
+            nodejs(configMap)
+            break
+        case "javaVm":
+            javaVm(configMap)
+            break
+        case "pythonVm":
+            pythonVM(configMap)
+            break
+        default:
+            error "these application pipeline is not found"
+    }
+}
+
+def decidePipeline(Map configMap){
+    application = configMap.getz("application")
+    switch(application){
+        case "nodejs":
+            nodejs(configMap)
+        case "pythonVM":
+            pythonVM(configMap)
+        case "javaVM":
+            javaVM(configMap)
+        default:
+            error "these application pipeline is not found"
+    }
+}
+
+def decidePipeline(Map configMap){
+    switch(application){
+        case "nodejs":
+            nodejs(configMap)
+            break
+        case "pythonVM":
+            python(configMap)
+            break
+        case ".netVM":
+            .netVM(configMap)
+            break
+    }
+
+}
